be9c30b30a4e7bd0eac77e5841e413f63b29cc57
timurzubcenko <timzub0@gmail.com>
2022-12-29 15:26:52 +0200

    first commit

--- src/pages/MainPage/MainPage.jsx
+++ src/pages/MainPage/MainPage.jsx
@@ -1,7 +1,6 @@
 import React from 'react'
 import Header from '../../components/header/Header';
 import Tasks from '../../components/tasks/Tasks';
-import styles from './MainPage.module.css'
 
 const MainPage = () => {
     return (
